b83d4da7e1e0035e82ff99a561c1b02453695d90
Jpwrbd <jpwrbd456@gmail.com>
2023-10-12 05:35:34 +0700

    up

--- Homework/HW12/hw12-2-sort-value.cpp
+++ Homework/HW12/hw12-2-sort-value.cpp
@@ -1,5 +1,5 @@
 /*
-    สร้าง Struct เพื่อเก็บข้อมูลของคน 2 คน โดยเก็บข้อมูล ชื่อ , อายุ , เพศ และคะแนนสะสม และแสดงผลออกมาบนหน้าจอ
+    สร้าง Struct เพื่อเก็บข้อมูลของคน 4 คน โดยเก็บข้อมูล ชื่อ , อายุ , เพศ และคะแนนสะสม และแสดงผลออกมาบนหน้าจอ
     
     Test case:
         Student A
@@ -60,3 +60,77 @@
         99.99 88.88 77.77 66.66
 
 */
+
+#include <stdio.h>
+struct Person {
+	    char  Name[20] ;
+	    float Score ;
+	} ;
+void student(int std){
+    if (std == 1)
+    {
+        printf( "Student A" ) ;
+    }else if (std == 2)
+    {
+        printf( "Student B" ) ;
+    }else if (std == 3)
+    {
+        printf( "Student C" ) ;
+    }else{
+        printf( "Student D" ) ;
+    }
+}
+
+int main(){
+    Person A[4] ;
+    for (int i = 0 ; i < 4 ; i++)
+    {
+        student(i+1);
+        printf( "\nName : \n" ) ;
+        scanf( "%s",&A[i].Name) ;
+        printf( "Score : \n") ;
+        scanf( "%f",&A[i].Score) ;
+    }
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 4 ; j++) {
+            if (A[i].Score < A[j].Score) {
+                struct Person Box = A[i] ;
+                A[i] = A[j] ;
+                A[j] = Box ;
+            }
+        }
+    }
+    for (int i = 0 ; i < 4 ; i++)
+    {
+        printf( "%s ", A[i].Name) ;
+    }
+    printf("\n") ;
+    for (int i = 0 ; i < 4 ; i++)
+    {
+        printf( "%.2f ", A[i].Score) ;
+
+       
+    }
+ 
+    
+    return 0 ;
+}
+
+//  printf("Person A\nName : \n") ;
+//     scanf("%s",A.Name) ;
+//     printf("Score : \n") ;
+//     scanf("%f",&A.Score) ;
+
+//     printf("Person B\nName : \n") ;
+//     scanf("%s",B.Name) ;
+//     printf("Score : \n") ;
+//     scanf("%f",&B.Score) ;
+
+//     printf("--| Person A Information |--\n") ;
+//     printf("Name : %s (%c)\n", A.Name) ;
+//     printf("Score : %.2f points\n", A.Score) ;
+
+//     printf("--| Person B Information |--\n") ;
+//     printf("Name : %s (%c)\n", B.Name) ;
+//     printf("Score : %.2f points\n", B.Score) ;
+    
